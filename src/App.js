9c17646d5f87d21d0c7ee69f853ee530c1f286ab
Webudvikler Hovedforløb <mich536x@elev.techcollege.dk>
2023-01-29 14:23:27 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from "styled-components";
+import { MainTheme } from "./Styles/Themes/MainTheme";
+import { GlobalStyles } from "./Styles/Global";
+import { Toggler } from "./Components/DarkmodeSwitch/Toggler";
+import { useDarkmodeStore } from "./Components/DarkmodeSwitch/useDarkmodeStore";
 
 function App() {
+  /** get value of theme from the themestore */
+  const { mode } = useDarkmodeStore();
+
+  /**set theme to dark or light */
+  const themeMode = mode === "light" ? "light" : "dark";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={MainTheme({ themeMode })}>
+      <Toggler />
+      <GlobalStyles />
+      <article>
+        <header>
+          <h1>Jeg er lige her</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti maiores, doloremque sapiente similique facere expedita nihil, autem eligendi commodi corrupti tempora perferendis vitae
+            aliquam ut modi animi hic odio aut!
+          </p>
+        </header>
+      </article>
+    </ThemeProvider>
   );
 }
 
